9627a4e3cf8f8e05c39dea68063a1a8f53070573
Stanislaw-K <nightelf2293@gmail.com>
2024-09-24 22:20:25 +0200

    the timer module is ready

--- project_1_classic_style/gulpfile.js
+++ project_1_classic_style/gulpfile.js
@@ -4,8 +4,8 @@ const gulp = require("gulp");
 const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
-// const dist = "./dist/";
-const dist = "F:/MAMP/htdocs/Project_window";
+const dist = "./dist/";
+// const dist = "F:/MAMP/htdocs/Project_window";
 
 gulp.task("copy-html", () => {
   return gulp.src("./src/index.html")
